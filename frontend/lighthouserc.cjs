cfa25ef9fad38f25de16299083c1dd87c917a454
carlos-cas3 <109805594+carlos-cas3@users.noreply.github.com>
2025-12-03 18:26:46 -0500

    Update lighthouserc.cjs

--- frontend/lighthouserc.cjs
+++ frontend/lighthouserc.cjs
@@ -1,7 +1,22 @@
 module.exports = {
   ci: {
     collect: {
-      staticDistDir: 'dist',
+      url: [
+        'http://localhost:8080/dashboard',
+        'http://localhost:8080/calendario',
+        'http://localhost:8080/actividades/examenes',
+        'http://localhost:8080/actividades/extras',
+        'http://localhost:8080/actividades/proyectos',
+        'http://localhost:8080/actividades/tareas',
+        'http://localhost:8080/profile',
+        'http://localhost:8080/settings',
+        'http://localhost:8080/notificaciones',
+        'http://localhost:8080/cargaSemanal',
+        'http://localhost:8080/reportes',
+        'http://localhost:8080/editProfile',
+        'http://localhost:8080/login',
+        'http://localhost:8080/register'
+      ]
     },
     upload: {
       target: 'temporary-public-storage',
